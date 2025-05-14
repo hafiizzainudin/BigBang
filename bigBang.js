bf58e7c40a44b01a44c1f07f2f738f304a600c67
Hafiiz Zainudin <148052390+hafiizzainudin@users.noreply.github.com>
2025-05-15 00:01:52 +0800

    Add files via upload

---
+++ bigBang.js
@@ -0,0 +1,36 @@
+// Import the built-in 'fs' module to enable file system operations like writing to a file
+const fs = require('fs');
+
+// Create an empty array to store the result
+const result = [];
+
+// Loop through numbers from 1 to 100
+for (let i = 1; i <= 100; i++) 
+{
+  // If the number is divisible by both 3 and 5 (i.e., divisible by 15), push "BIGBANG"
+  if (i % 42 === 0) 
+  {
+    result.push("BIGBANG");
+  } 
+  // If the number is divisible by only 3, push "BIG"
+  else if (i % 6 === 0) 
+  {
+    result.push("BIG");
+  } 
+  // If the number is divisible by only 5, push "BANG"
+  else if (i % 7 === 0) 
+  {
+    result.push("BANG");
+  } 
+  // If the number is not divisible by 3 or 5, push the number as a string
+  else 
+  {
+    result.push(i.toString());
+  }
+}
+
+// Convert the result array to JSON format and write it to a file named 'output.json'
+fs.writeFileSync('output.json', JSON.stringify(result, null, 2));
+
+// Print a message to the console to indicate the file has been created
+console.log('output.json has been created!');
